e51bcc2632a54deac67f753ef4aa66c0c97634da
Vijay balaji <balajibaskaran456@gmail.com>
2026-07-09 13:08:32 +0530

    feat:Demo products feature added

--- src/main/java/com/demo/application/controller/ProductController.java
+++ src/main/java/com/demo/application/controller/ProductController.java
@@ -1,11 +1,25 @@
 package com.demo.application.controller;
 
+import com.demo.application.dtos.DemoProducts;
+import com.demo.application.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
-    private final
+    private final ProductService productService;
+
+    public ProductController(ProductService productService){
+        this.productService=productService;
+    }
+    @GetMapping("/demo-products")
+    public ResponseEntity<List<DemoProducts>> getAllDemoProducts(){
+        return ResponseEntity.ok(productService.getAllDemoProducts());
+    }
 }
