194aefabb91b8e106d0afc605c0490782aea6fce
Vijay balaji <balajibaskaran456@gmail.com>
2026-07-10 16:14:07 +0530

    security update for jwt

--- src/main/java/com/demo/application/security/ConfigSecurity.java
+++ src/main/java/com/demo/application/security/ConfigSecurity.java
@@ -1,15 +1,30 @@
 package com.demo.application.security;
 
+import com.demo.application.filter.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class ConfigSecurity {
+
+    private final JwtFilter jwtFilter;
+    public ConfigSecurity(JwtFilter jwtFilter){
+        this.jwtFilter=jwtFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -22,11 +37,24 @@ public class ConfigSecurity {
                                 .requestMatchers(HttpMethod.POST,"/api/add-user").permitAll()
                                 .requestMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated()
                                 .requestMatchers("/").permitAll()
-                ).build();
+                ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
     }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(bCryptPasswordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
