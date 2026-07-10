fd69a8b5f9694faed307a3b77a8d87e9765735b0
Dhinakaran <dhinabruce461@gmail.com>
2026-07-10 15:29:02 +0530

    feat : security added

---
+++ src/main/java/com/demo/application/security/ConfigSecurity.java
@@ -0,0 +1,32 @@
+package com.demo.application.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ConfigSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth->auth
+                                .requestMatchers(HttpMethod.GET,"/api/profile/*").authenticated()
+                                .requestMatchers(HttpMethod.GET,"/api/ud/").hasRole("ADMIN").anyRequest().authenticated()
+                                .requestMatchers("/api/ud/**").authenticated()
+                                .requestMatchers(HttpMethod.POST,"/api/add-user").permitAll()
+                                .requestMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated()
+                                .requestMatchers("/").permitAll()
+                ).build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
